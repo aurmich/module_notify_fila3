eb327d44fd3c19cbcfc3e8cbaf8364d4754d2e40
marco76tv <marco.sottana@gmail.com>
2025-03-07 16:02:04 +0100

    Merge commit '6e74f4bee5c82b0352a6c6691c2a827baa7a2e3c' as 'laravel/Modules/Quaeris'

---
+++ app_old/Filament/Resources/PdfStyleResource/Pages/EditPdfStyle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\Quaeris\Filament\Resources\PdfStyleResource\Pages;
+
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+use Modules\Quaeris\Filament\Resources\PdfStyleResource;
+
+class EditPdfStyle extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
+{
+    protected static string $resource = PdfStyleResource::class;
+
+    
+}
