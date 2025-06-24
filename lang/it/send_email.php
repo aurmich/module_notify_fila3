ad548300c1521e7d5b6c14fd968c46b401525e25
marco76tv <marco.sottana@gmail.com>
2025-06-24 13:31:05 +0200

    .

--- lang/it/send_email.php
+++ lang/it/send_email.php
@@ -60,15 +60,10 @@ return array (
     ),
     'subject' => 
     array (
-<<<<<<< HEAD
       'description' => 'Oggetto email',
       'helper_text' => 'Inserisci l\'oggetto dell\'email',
       'label' => 'Oggetto',
     )
-=======
-      'description' => 'subject',
-    ),
->>>>>>> c895250 (.)
   ),
   'actions' => 
   array (
